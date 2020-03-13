73e6fc85667cb27c08ded810c8dd06c3bf80be28
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-03-13 08:24:41 +0200

    Queue WIP

--- fullstack/1/server/mq/rabbitmq.py
+++ fullstack/1/server/mq/rabbitmq.py
@@ -3,7 +3,9 @@ import pika
 
 class RabbitMQProvider:
     def post(self, msg):
-        connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+        credentials = pika.PlainCredentials("admin", "password")
+        parameters = pika.ConnectionParameters("rabbitmq", 5672, "/", credentials)
+        connection = pika.BlockingConnection(parameters)
         channel = connection.channel()
         channel.queue_declare(queue="items")
         channel.basic_publish(exchange="", routing_key="items", body=msg)
