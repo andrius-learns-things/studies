6c0613d9bf20ddf95b7f1d36dc712bda75249035
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-03-14 10:37:58 +0200

    Celery WIP

--- fullstack/1/server/worker.py
+++ fullstack/1/server/worker.py
@@ -1,12 +1,12 @@
 from celery import Celery
 
 
-app = Celery(broker="amqp://admin:password@rabbitmq:5672", include=["tasks"])
+app = Celery(broker="amqp://admin:password@rabbitmq:5672")
 
 
-app.conf.beat_schedule = {"refresh": {"task": "add_item_async", "schedule": 30.0}}
+app.conf.beat_schedule = {"add_item": {"task": "add_item_async", "schedule": 30.0}}
 
 
 @app.task(bind=True, name="add_item_async")
-def add_item_async(self, urls):
+def add_item_async(self):
     print("Doing smth")
