248321d03457634cccb145103ea83dff40ab833d
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-03-21 10:23:57 +0200

    Endpoint to add item

--- fullstack/2/server/items_app/views.py
+++ fullstack/2/server/items_app/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from .serializers import ItemSerializer
 from .models import Item
 
@@ -7,6 +8,15 @@ from .models import Item
 @csrf_exempt
 def item_list(request):
     if request.method == "GET":
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return _get_all()
+
+    if request.method == "POST":
+        item = Item(name="New item")
+        item.save()
+        return _get_all()
+
+
+def _get_all():
+    items = Item.objects.all()
+    serializer = ItemSerializer(items, many=True)
+    return JsonResponse(serializer.data, safe=False)
