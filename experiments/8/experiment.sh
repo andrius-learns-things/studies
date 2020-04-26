d633066af98f6028f8fc5924c4cb73b08a07d189
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-04-26 13:59:55 +0300

    Memory leaks WIP

--- experiments/8/experiment.sh
+++ experiments/8/experiment.sh
@@ -2,9 +2,9 @@
 
 docker-compose up -d
 
-docker-compose run tester python /code/tester.py -url http://web
+docker-compose run tester python /code/tester.py -url http://web -times 10
 
-docker-compose run tester python /code/tester.py -url http://web/use-and-release-1kb
+docker-compose run tester python /code/tester.py -url http://web/use-and-release-1kb -times 10
 
 
 # curl localhost:8059
--- experiments/8/tester/code/tester.py
+++ experiments/8/tester/code/tester.py
@@ -1,12 +1,24 @@
 import fire
 import requests
+import time
 
 
-def measure_http_request(url):
+def measure_http_request(url, times=1):
 
-    r = requests.get(url)
+    start = time.time()
 
-    return "HTTP {} : {}".format(r.status_code, r.text)
+    for i in range(times):
+        do_reqest(url)
+
+    end = time.time()
+
+    avg = 1000 * (end - start) / times
+
+    return "{:.1f}ms on avg for {} requests to {}.".format(avg, times, url)
+
+
+def do_reqest(url):
+    requests.get(url)
 
 
 if __name__ == "__main__":
