b26c0570d7d30468050b2a79cebe543f7d0e5895
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-04-28 12:55:23 +0300

    Memory leaks WIP

--- experiments/8/experiment.sh
+++ experiments/8/experiment.sh
@@ -14,6 +14,13 @@ docker-compose run tester python /code/tester.py -url http://web/use-and-release
 
 docker-compose ps -q web | xargs  docker stats --no-stream
 
+docker-compose run tester python /code/tester.py -url http://web/use-and-leak-1kb -times 100
+
+docker-compose ps -q web | xargs  docker stats --no-stream
+
+
+
+
 # curl localhost:8059
 # docker stats $(docker-compose ps -q)
 # docker ps -q | xargs  docker stats --no-stream
--- experiments/8/web/app.py
+++ experiments/8/web/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+ONE_KB_STRING = "__________".replace("_", "----------").replace("-", "__________")
+
 
 @app.route("/")
 def empty():
@@ -10,4 +12,17 @@ def empty():
 
 @app.route("/use-and-release-1kb")
 def use_and_release_one_kb():
-    return "Endpoint, which uses and releases 1KB of memory"
+    a = ONE_KB_STRING + ""
+    return "Endpoint, which uses and releases 1KB of memory. {}".format(len(a))
+
+
+leaked = []
+
+
+@app.route("/use-and-leak-1kb")
+def use_and_leak_one_kb():
+    a = ONE_KB_STRING + ""
+    leaked.append(a)
+    return "Endpoint, which uses and leaks 1KB of memory. Leaked: {}".format(
+        len(leaked)
+    )
