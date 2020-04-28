10b3ca8582e569f4053b2a5cc5bc1e12fe4a366b
Andrius Studies Things <andrius.studies.things@gmail.com>
2020-04-28 12:21:22 +0300

    Mem leaks WIP

--- experiments/8/experiment.sh
+++ experiments/8/experiment.sh
@@ -2,10 +2,19 @@
 
 docker-compose up -d
 
-docker-compose run tester python /code/tester.py -url http://web -times 10
+docker-compose restart web
 
-docker-compose run tester python /code/tester.py -url http://web/use-and-release-1kb -times 10
+docker-compose ps -q web | xargs  docker stats --no-stream
 
+docker-compose run tester python /code/tester.py -url http://web -times 100
+
+docker-compose ps -q web | xargs  docker stats --no-stream
+
+docker-compose run tester python /code/tester.py -url http://web/use-and-release-1kb -times 100
+
+docker-compose ps -q web | xargs  docker stats --no-stream
 
 # curl localhost:8059
 # docker stats $(docker-compose ps -q)
+# docker ps -q | xargs  docker stats --no-stream
+# docker-compose logs web
